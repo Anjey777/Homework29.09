ac6986baa89ed97a8ef3dc40e72cf930a8ec6a48
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-10-05 21:34:03 +0300

    34 finish

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -11,14 +11,15 @@ int n = int.Parse(Console.ReadLine());
 int [] arr = new int[n];
 Random rnd = new Random();
 for (int i = 0; i < arr.GetLength(0); i++)
+{
 arr[i] = rnd.Next(100, 999);
-Console.WriteLine("Ваш случайный массив:");
-for (int i = 0; i < arr.GetLength(0); i++, Console.WriteLine())
-Console.WriteLine ( arr[i]);
-
+Console.WriteLine($"Число Вашего массива:"+ arr[i]);
+}
 int even;
 even = 0;
 for (int i = 0; i < arr.GetLength(0); i++)
+{
                    if (arr[i] % 2 == 0)
                   even++;
+}
 Console.WriteLine($"Количество четных: {even}");
