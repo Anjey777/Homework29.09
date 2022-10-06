4f2ed707c530a8bef6c1cc9b89e8511c4f2955a5
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-10-06 19:40:39 +0300

    finish

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -3,50 +3,26 @@
 Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76
 */
-/*
-int size = 5;
-int[] arr = new int[size];
+
+
 Random rnd = new Random();
-for (int i = 0; i < arr.GetLength(0); i++)
+int[] mas = new int[10];
+int max = 0;
+int min = 99;
+for (int i = 0; i < mas.Length; i++)
 {
-arr[i] = rnd.Next(1, 100);
-Console.WriteLine(+ arr[i]);
+    mas[i] = rnd.Next(1, 99);
 }
-*/
-            int  i, n;
-            int[] m = new int[100];
- 
-            Random rand = new Random();
-            n = 5;
-            Console.WriteLine("Ваш массив: " );
-            for (i = 0; i < n; i++)
-            {
-                m[i] = rand.Next(0, 50);
-                Console.Write(m[i] + " ");
-            }
-            var min = m[i];
-            var max = m[i];
- 
-            for (i = 0; i < n; i++)
-            {
-                if (m[i] < m[min])
-                {
-                    min =i;
-                }
-                else if (m[i] > m[max])
-                {
-                    max =i;
-                }
-            }
-            {
-            Console.WriteLine("/Минимальный элемент: " + m[min]);
-            Console.Write("/Максимальный элемент: " + m[max]);
-            for (i = 0; i < n; i++);
-            }
-            //Console.Write(m[i] + " ");
-            
-            Console.WriteLine("\n Элементы между минимум и максимом");
-            for (i = min+1; i<max; i++); 
-            //Console.WriteLine(m[i] " ");
-            
- 
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i] > max) max = mas[i];
+    if (mas[i] < min) min = mas[i];
+}
+for (int i = 0; i < mas.Length; i++)
+{
+    Console.Write(" " + mas[i]);
+    Console.WriteLine();
+    Console.WriteLine("Минимальный элемент:  " + max);
+    Console.WriteLine("Минимальный элемент:  " + min);
+    Console.WriteLine("Разница между элементами:  " + (max - min));
+}
